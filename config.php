147a3ead9a0a59c3b0f41ff5c362ed76fbd62561
serhii <zyozia@gmail.com>
2016-10-15 14:39:14 +0300

    test host

--- config.php
+++ config.php
@@ -1,11 +1,21 @@
-<?php
+u485438947_zyozi<?php
 //Задаем константы:
 define ('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
 $sitePath = realpath(dirname(__FILE__) . DS);
 define ('SITE_PATH', $sitePath); // путь к корневой папке сайта
  
 // для подключения к бд
+define('DB_USER', 'u485438947_zyozi');
+define('DB_PASS', '123789');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'u485438947_cbook');
+
+/*
+
+// для подключения к бд
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'complaintbook');
+
+*/

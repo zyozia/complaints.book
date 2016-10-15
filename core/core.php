a483dfabaa7c21f62c294a25de3baba1fcd4b2d8
serhii <zyozia@gmail.com>
2016-10-15 15:28:28 +0300

    test host

--- core/core.php
+++ core/core.php
@@ -24,8 +24,8 @@ function __autoload($className) {
     // путь до класса
     //$file = SITE_PATH .DS. $folder . DS . $filename;
     $file = SITE_PATH . $folder . DS . $filename;
-	echo $file;
-   // echo $file."<br>";
+	
+    echo $file."<br>";
     // проверяем наличие файла
     if (file_exists($file) == false) {
         return false;
